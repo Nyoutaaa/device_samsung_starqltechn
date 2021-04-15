ea6f455cd09f2142b09832dedd43ac7e79d774af
Nyoutaaa <shhsdsjshs@gmail.com>
2021-04-14 21:02:46 -0300

    starqltechn: DotOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_starqltechn.mk
+    $(LOCAL_DIR)/dot_starqltechn.mk
 
 COMMON_LUNCH_CHOICES := \
-	aosp_starqltechn-user \
-	aosp_starqltechn-userdebug \
-	aosp_starqltechn-eng
+	dot_starqltechn-user \
+	dot_starqltechn-userdebug \
+	dot_starqltechn-eng
--- dot_starqltechn.mk
+++ dot_starqltechn.mk
@@ -22,11 +22,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 # Inherit from starqltechn device
 $(call inherit-product, device/samsung/starqltechn/device.mk)
 
-# Inherit some common AOSPK stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common DotOS stuff
+$(call inherit-product, vendor/dot/config/common.mk)
+
+# DotOS Official
+DOT_OFFICIAL := true
 
 # Device identifier
-PRODUCT_NAME := aosp_starqltechn
+PRODUCT_NAME := dot_starqltechn
 PRODUCT_DEVICE := starqltechn
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G9600
